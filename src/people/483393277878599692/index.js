42d1c5e65a67aa92abc699d57479d6bcc84e8a7b
Ryan Yocum <rtyocum1@gmail.com>
2021-01-29 23:01:01 -0500

    Removed Jason Druelo

--- src/people/483393277878599692/index.js
+++ src/people/483393277878599692/index.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const fsp = require('fs').promises;
 const path = require('path');
+const { promisify } = require('util');
+const sleep = promisify(setTimeout);
 
 module.exports.run = async (dc, oldState, newState) => {
   const conn = await newState.channel.join();
   let dir = path.join(__dirname, 'clips');
   let files = await fsp.readdir(dir);
   let file = files[Math.floor(Math.random() * files.length)]
+  await sleep(1000);
   conn.play(fs.createReadStream(path.join(dir, file)), { volume: 0.5 });
-  setTimeout(() => {
-    conn.disconnect();
-  }, 10000);
+  await sleep(10000);
+  conn.disconnect();
 }
